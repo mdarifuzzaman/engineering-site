0f1d54475ee5406b74405ea899e0add7e3c373f4
Arif Uzzaman <arif_uap@outlook.com>
2024-06-12 23:21:06 +1000

    added service meta

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Our Services",
+    description: "Traffic Engineering | Feature and Level Surveys | Civil Engineering",
+  };
 
 const Page = async () => {
     return(
